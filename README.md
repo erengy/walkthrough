c29db0210587853a5e893cf70c9f1d0159a86168
Eren Okka <erenokka@gmail.com>
2014-01-20 00:14:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Visual novel guides with modern features:
+
+- Easier to follow than regular walkthroughs
+- Presents the option to hide upcoming choices to avoid potential spoilers
+- Functions well on both desktop and mobile environments
+- Reusable in multiple projects
---
+++ js/walkthrough.js
@@ -0,0 +1,203 @@
+var walkthroughData = null;
+
+var walkthroughOptions = {
+  hideSpoilers: true
+}
+
+function loadWalkthrough(file) {
+  $.ajax({
+    'async': false,
+    'global': false,
+    'url': file,
+    'dataType': 'json',
+    'success': function (data) {
+      walkthroughData = data;
+      initDocument();
+    }
+  });
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function initDocument() {
+  var title = walkthroughData.meta.title + ' Walkthrough';
+  document.title = title;
+  $('#header #title').html(title);
+  $('meta[name=author]').attr('content', walkthroughData.meta.author);
+  $('#header #description').html(walkthroughData.meta.description);
+
+  createNavigationList();
+}
+
+function applySpoilerOption() {
+  walkthroughOptions.hideSpoilers = $('#options #spoilers').prop('checked');
+
+  var next = $('#main #choices tbody .next');
+
+  if (walkthroughOptions.hideSpoilers) {
+    next.nextAll().addClass('hidden');
+  } else {
+    next.siblings().removeClass('hidden');
+  }
+}
+
+function trackProgress(element) {
+  // Mark previous lines
+  $(element).addClass('previous');
+  $(element).prevAll().addClass('previous');
+  $(element).nextAll().removeClass('previous');
+
+  // Mark next line
+  $(element).removeClass('next');
+  $(element).siblings().removeClass('next');
+  $(element).next().addClass('next');
+
+  $(element).removeClass('hidden');
+  $(element).siblings().removeClass('hidden');
+  if (walkthroughOptions.hideSpoilers) {
+    // Hide all future lines except the next one
+    $(element).nextAll().addClass('hidden');
+    $(element).next().removeClass('hidden');
+    // Make sure the next line is in view
+    $(element).next().get(0).scrollIntoView();
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function createNavigationList() {
+  var routes = walkthroughData.routes;
+
+  $('#nav').removeClass('hidden');
+
+  for (var i = 0; i < routes.length; i++) {
+    $('#nav #routes').append('<li><a href="#" id="' + routes[i].id + '">' +
+                             routes[i].title + '</a></li>');
+  }
+}
+
+function createMainContent(routeId) {
+  var routeIndex = findRouteIndexById(routeId);
+  var route = walkthroughData.routes[routeIndex];
+
+  $('#main').removeClass('hidden');
+
+  $('#main #title').html(route.title);
+
+  if (route.description) {
+    $('#main #description').html('<p>' + route.description + '</p>');
+  } else {
+    $('#main #description').html('');
+  }
+
+  createChoiceTable(route);
+}
+
+function createChoiceTable(routeParent) {
+  var route = routeParent.route;
+
+  if (!route) {
+    $('#main #choices').html('');
+    return;
+  }
+
+  var table = $('<table>');
+
+  // Header
+  table.append('<thead>');
+  $('thead', table).append('<tr>');
+  $('thead tr', table).append(
+      $('<th>', {'style': 'width: 25%'}));
+  $('thead tr', table).append(
+      $('<th>', {'style': 'width: 75%',
+                 html: '<span class="route">Choices</span>'}));
+
+  // Body
+  table.append('<tbody>');
+  for (var i = 0; i < route.length; i++) {
+    var date = route[i].date;
+    var choices = route[i].choices;
+    for (var j = 0; j < choices.length; j++) {
+      var tr = $('<tr>');
+      tr.append('<td>');
+      if (j == 0 && date) {
+        tr.attr('class', 'first');
+        $('td', tr).append(makeSpan('date', date));
+      }
+      tr.append('<td>' + getTableCellData(choices[j]) + '</td>');
+      $('tbody', table).append(tr);
+    }
+  }
+
+  $('#main #choices').html(table);
+  $('#main #choices table tbody tr').first().addClass('next');
+}
+
+function findRouteIndexById(routeId) {
+  var routes = walkthroughData.routes;
+
+  for (var i = 0; i < routes.length; i++) {
+    if (routes[i].id === routeId) {
+      return i;
+    }
+  }
+
+  return null;
+}
+
+function getTableCellData(object) {
+  var html = '';
+
+  // Array
+  if (Array.isArray(object)) {
+    for (var i = 0; i < object.length; i++) {
+      if (html !== '') {
+        html += '<br/>';
+      }
+      html += getTableCellData(object[i]);
+    }
+
+  // Object
+  } else if (typeof object === 'object') {
+    if (object.save) {
+      html += makeSpan('alert', '(Create Save ' + object.save + ')');
+    } else if (object.load) {
+      html += makeSpan('alert', '(Load Save ' + object.load + ')');
+    } else if (object.end) {
+      html += makeSpan('alert', '(' + object.end + ')');
+    } else {
+      if (object.choice) {
+        html += object.choice;
+      }
+      if (object.comment) {
+        html += makeSpan('comment', ' (' + object.comment + ')');
+      }
+    }
+
+  // String
+  } else {
+    html += object;
+  }
+
+  return html;
+}
+
+function makeSpan(className, text) {
+  return '<span class="' + className + '">' + text + '</span>';
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Event handlers
+
+$('#nav ul').on('click', 'li a', function() {
+  createMainContent(this.id);
+  applySpoilerOption();
+});
+
+$('#options').on('click', '#spoilers', function() {
+  applySpoilerOption();
+});
+
+$('#main').on('click', '#choices table tbody tr', function() {
+  trackProgress(this);
+});
